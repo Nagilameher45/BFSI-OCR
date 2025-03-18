6adc66f039f5b97a89398d6f07d0e470b6a78a05
Nagilameher45 <nagila.26meher@gmail.com>
2025-03-18 22:47:18 +0530

    Delete bfsi_ocr_website directory

--- bfsi_ocr_website/ocr.py
+++
@@ -1,9 +0,0 @@
-import pytesseract
-import cv2
-import sys
-
-def extract_text(image_path):
-    img = cv2.imread(image_path)
-    text = pytesseract.image_to_string(img)
-    print(text)  # This will be read by Node.js
-    sys.stdout.flush()
